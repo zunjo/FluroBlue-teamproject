860072503063a2038ccdfb8814b0adbead4048ff
ntansey <nvtansey@hotmail.com>
2015-01-17 20:29:33 +1100

    Added: Prefabs, scripts, 5 scenes etc
    Note: Can't get the visual effect linked to the objective points to turn off when tagged. Couldn't work out the coding for it - may want to replace with alternate graphic?

---
+++ MainProj/Assets/Scripts/Move.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public bool standing;
+	private bool jumping;
+	public float upforce;
+	public float forwardforce;
+	public float mspeed; 
+	public float tspeed; 
+
+	void Start()
+	{
+		standing = true;
+		jumping = false;
+		upforce = 500;
+		forwardforce = 500;
+		mspeed = 10;
+		tspeed = 150;
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if (coll.transform.tag == "Ground") 
+		{
+			jumping = false;
+			standing = true;
+		}
+		if (coll.transform.tag == "NavPoint") 
+		{
+			jumping = false;
+			standing = true;
+			coll.transform.tag = "Ground";
+			GameObject navPoint = GameObject.Find ("Scorer");
+			ScorerScript navTotal = navPoint.GetComponent<ScorerScript> ();
+			navTotal.SubNavPoints ();
+		}
+		if (coll.transform.tag == "Enemy") 
+		{
+			GameObject deadplayer = GameObject.FindGameObjectWithTag ("Player"); // kills the player
+			Destroy (deadplayer);
+		}
+		if (coll.transform.tag == "Water") 
+		{
+			GameObject deadplayer = GameObject.FindGameObjectWithTag ("Player"); // kills the player
+			Destroy (deadplayer);
+		}
+	}
+
+	void Update ()
+	{
+		if (standing) 
+		{
+			float y = Input.GetAxis ("Horizontal") * tspeed * Time.deltaTime;
+			transform.Rotate (0, y, 0);
+			float x = Input.GetAxis ("Vertical") * mspeed * Time.deltaTime;
+			transform.Translate (x, 0, 0);
+		}
+
+		if (Input.GetKeyDown ("space") && standing == true) 
+		{
+			rigidbody.velocity.Set (0, 0, 0);
+			jumping = true;
+		} 
+
+		if (jumping) 
+		{
+			rigidbody.AddForce (transform.up * upforce);
+			rigidbody.AddForce (transform.right * forwardforce);
+			standing = false;
+			jumping = false;
+		}
+
+	}
+}
---
+++ MainProj/Assets/Scripts/ScorerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScorerScript : MonoBehaviour {
+
+	public int NavPointsLeft = 0;
+
+	public void AddNavPoints ()
+	{
+		NavPointsLeft = 1 + NavPointsLeft;
+	}
+
+	public void SubNavPoints ()
+	{
+		NavPointsLeft = NavPointsLeft - 1;
+	}
+
+	void OnGUI()
+	{
+		Rect position = new Rect(0,0, 100, 100);
+		GUI.color = Color.red;
+		GUI.Label(position, "Points Left: " + NavPointsLeft.ToString());
+	}
+}
